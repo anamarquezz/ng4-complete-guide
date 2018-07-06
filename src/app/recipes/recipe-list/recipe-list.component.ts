936f87d74d2685f837a6de21f1597fd0e3aa375a
ana_marquez <anamarquez.edu@hotmail.com>
2018-07-05 17:07:50 -0700

    TUTORIAL update

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','this is a simple a test','https://www.chowstatic.com/assets/2015/05/31440_classic_pesto_salad.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
